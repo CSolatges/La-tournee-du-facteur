16acf591fa567ddb2e21b292da1654ac8dfe459f
Constance <constance.solatges@insa-rouen.fr>
2023-02-10 11:09:01 +0100

    Euler

--- Euler.py
+++ Euler.py
@@ -1,5 +1,62 @@
 ## L'objectif de ce code est de déterminer les cycles Eulérien d'un graphe par l'algorithme d'Euler
 
-#Ecrire une m ethode qui, étant donnés deux sommets de degrés impairs x et y dans un graphe G
-#connexe qui possède exactement deux sommets de degrés impairs, retourne une chaîne d’origine x
-#et d’arrivée y et supprime les arêtes qui la constituent dans G.
+#Écrire une méthode qui retourne le nombre de sommets de degrés impairs dans un graphe.77
+
+import numpy as np
+
+#Basé sur le TD algo graphes eulériens
+
+
+"Cette partie permet de déterminer si un graphe admet un cycle ou une chaîne eulerienne"
+
+def nbimpaires(G):
+    imp=0
+    for sommet in G: #parcours les sommets de G
+        arrete=0
+        for i in len(sommet): #on condère que les sommets sontdes listes remplis de 1 et de 0
+            arrete+=i
+        if arrete%2==1:
+            imp+=1 
+    return (imp)
+
+def cycleul(G): 
+    #Un graphe connexe admet un cycle eulérien si et seulement si il ne possède aucun sommet de degré impair.
+    nbimp=nbimpaires(G)
+    if nbimp==0:
+        return True
+    else:
+        return False
+    
+def chaineul(G):
+    #Un graphe connexe admet une chaîne eulérien si et seulement si il ne possède 0 ou 2 de degré impair.
+    nbimp=nbimpaires(G)
+    if nbimp==0:
+        return True
+    elif nbimp==2:
+        return True
+    else:
+        return False
+    
+
+"Cette partie permet de déterminer un cycle Eulérien"
+
+def cycleulerien(G,s):
+    #fonction qui retourne une cycle eulérien dans le graphe G en partant d'un sommet S
+    A=[]
+    for i in range(len(s)):
+        if s[i]!=0:
+            A.append(i) 
+    if A==[]:
+        return ([s])
+    else:
+        C=[] #un cycle quelconque dans G 
+        #ici on va supprimer les arrêtes de ce cycle dans G
+        #Je sais pas encore comment faire ça
+        l=len(C)
+        R=[]
+        for i in range(l):
+            R.append(cycleulerien(G,C[i]))
+        return (R)
+    
+        
+
--- test_insa.py
+++ test_insa.py
@@ -6,14 +6,17 @@ import Euler as E
 import Hierholzer as H
 
 #Matrice d'adjacence de notre graphe
-GINSA= np.array([[np.inf,1,np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,np.inf],[1,np.inf,1,np.inf,np.inf,np.inf,np.inf,1,np.inf], [np.inf,1,np.inf,1,np.inf,np.inf,1,np.inf,np.inf], [np.inf,np.inf,1,np.inf,1,1,np.inf,np.inf,np.inf],[np.inf,np.inf,np.inf,1,np.inf,np.inf,np.inf,np.inf,np.inf],[np.inf,np.inf,np.inf,1,np.inf,np.inf,1,np.inf,np.inf],[np.inf,np.inf,1,np.inf,np.inf,1,np.inf,1,1],[np.inf,1,np.inf,np.inf,np.inf,np.inf,1,np.inf,1],[np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,1,1,np.inf]])
-#1 quand il y a une arrête et +inf sinon
+GINSA= np.array([[0,1,0,0,0,0,0,0,0],[1,0,1,0,0,0,0,1,0], [0,1,0,1,0,0,1,0,0], [0,0,1,0,1,1,0,0,0],[0,0,0,1,0,0,0,0,0],[0,0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,1,1],[0,1,0,0,0,0,1,0,1],[0,0,0,0,0,0,1,1,0]])
+#1 quand il y a une arrête et 0 sinon
+
+GINSAdist= np.array([[np.inf,1,np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,np.inf],[1,np.inf,1,np.inf,np.inf,np.inf,np.inf,1,np.inf], [np.inf,1,np.inf,1,np.inf,np.inf,1,np.inf,np.inf], [np.inf,np.inf,1,np.inf,1,1,np.inf,np.inf,np.inf],[np.inf,np.inf,np.inf,1,np.inf,np.inf,np.inf,np.inf,np.inf],[np.inf,np.inf,np.inf,1,np.inf,np.inf,1,np.inf,np.inf],[np.inf,np.inf,1,np.inf,np.inf,1,np.inf,1,1],[np.inf,1,np.inf,np.inf,np.inf,np.inf,1,np.inf,1],[np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,1,1,np.inf]])
+#1 (ou poind de l'arrête) quand il y a une arrête et +inf sinon
 
 "Module Dijkstra"
 
 #Test de la fonction dijkstreDist
-print(D.dijkstraDist(GINSA, 0)) 
+print(D.dijkstraDist(GINSAdist, 0)) 
 # affiche une liste de liste avec [[distance minimal, sommet visité, sommet précédent]]
 
 #Test de la fonction dijkstraPCC
-print(D.dijkstraPCC(GINSA,0,8))
+print(D.dijkstraPCC(GINSAdist,0,8))
